bdbe7d0086d3c7d8492eac043176e3257cc89b2d
hahahaday12 <hahahaday12@naver.com>
2023-04-17 13:45:21 +0900

    💄style: layout  style 파일 분리

---
+++ src/styles/DetailStyle/Layout/layout.js
@@ -0,0 +1,10 @@
+import { css } from 'styled-components';
+
+export const MediaImg = css`
+  width: 80px;
+  height: 80px;
+  display: flex;
+  position: relative;
+  bottom: 20px;
+  margin-left: 40px;
+`
