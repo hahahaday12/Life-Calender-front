e64a70c9db30ef2c81333f6506f1e1c96df20390
hahahaday12 <hahahaday12@naver.com>
2023-09-12 21:29:28 +0900

    fix: placeholder 에 test 계정 작성

--- src/components/Form/LoginForm/login.js
+++ src/components/Form/LoginForm/login.js
@@ -1,18 +1,16 @@
-import { useState } from 'react'
-import styled from 'styled-components'
-import  { useNavigate }  from 'react-router-dom';
-import useInputs from '../../../hooks/useInputs'
+import { useState } from 'react';
+import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+import useInputs from '../../../hooks/useInputs';
 import AuthService from '../../../service/auth.service';
 import TokenRepository from '../../../repository/TokenRepository';
 import { InnerPlaceHolder } from '../../../styles/Common/CommonStyle';
 import { fontsize } from '../../../styles/Media/theme';
-import {ShowAlert} from '../../../pages/alert';
+import { ShowAlert } from '../../../pages/alert';
 import { faLastfmSquare } from '@fortawesome/free-brands-svg-icons';
 
-
 const LoginInput = () => {
-
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
 
   const [{ email, password }, onChangeForm] = useInputs({
     email: '',
@@ -21,93 +19,92 @@ const LoginInput = () => {
 
   const handleEnter = (e) => {
     if (e.key === 'Enter') {
-      if(!e.shiftKey){
-        HandleLogin(); 
+      if (!e.shiftKey) {
+        HandleLogin();
       }
     }
-  }
-
-   const  HandleLogin  = async () => {
-    if(email === undefined || email === ""  || email === null){
-      ShowAlert('이메일을 입력해주세요.', "warning", "확인")
-      return false;
-    }
-
-    if(password === undefined || password === ""  || password === null){
-      ShowAlert('비밀번호를 입력해주세요.', "warning", "확인")
-      return false;
-    }
-    
-    try {
-        await AuthService.Login({ id:email, password })
-        .then((res) => {
-          if (res.status === 201) {
-            TokenRepository.setToken(res);
-            if (TokenRepository.getToken()) {
-              ShowAlert('로그인되었습니다.', "success", "확인").then((isConfirmed) => {
-                if(isConfirmed){
-                  navigate('/thirdpage')
-                }
-              });
-            }
-          }
-        })
-    } catch (error) {
-      ShowAlert('로그인 실패했습니다.', "warning", "확인").then((isConfirmed) => {
-        return false;
-      });
-    };
   };
 
-    return(
-        <LoginForm>
-          <Inputbox>
-            <input
-              placeholder="이메일을 입력하세요"
-              autoComplete="on"
-              name={'email'}
-              onChange={onChangeForm}
-              />
-          </Inputbox>
-          <Inputbox>
-            <input
-              type={'password'}
-              placeholder="비밀번호를 입력하세요"
-              autoComplete="on"
-              name={'password'}
-              onChange={onChangeForm}
-              onKeyDown={handleEnter} 
-              />
-          </Inputbox>
-            <InerButton onClick={HandleLogin}>
-              로그인
-            </InerButton>
-        </LoginForm>
-    );
+  const HandleLogin = async () => {
+    if (email === undefined || email === '' || email === null) {
+      ShowAlert('이메일을 입력해주세요.', 'warning', '확인');
+      return false;
+    }
+
+    if (password === undefined || password === '' || password === null) {
+      ShowAlert('비밀번호를 입력해주세요.', 'warning', '확인');
+      return false;
+    }
+
+    try {
+      await AuthService.Login({ id: email, password }).then((res) => {
+        if (res.status === 201) {
+          TokenRepository.setToken(res);
+          if (TokenRepository.getToken()) {
+            ShowAlert('로그인되었습니다.', 'success', '확인').then(
+              (isConfirmed) => {
+                if (isConfirmed) {
+                  navigate('/thirdpage');
+                }
+              }
+            );
+          }
+        }
+      });
+    } catch (error) {
+      ShowAlert('로그인 실패했습니다.', 'warning', '확인').then(
+        (isConfirmed) => {
+          return false;
+        }
+      );
+    }
+  };
+
+  return (
+    <LoginForm>
+      <Inputbox>
+        <input
+          placeholder="test12@gmail.com"
+          autoComplete="on"
+          name={'email'}
+          onChange={onChangeForm}
+        />
+      </Inputbox>
+      <Inputbox>
+        <input
+          type={'password'}
+          placeholder="Test123456789!"
+          autoComplete="on"
+          name={'password'}
+          onChange={onChangeForm}
+          onKeyDown={handleEnter}
+        />
+      </Inputbox>
+      <InerButton onClick={HandleLogin}>로그인</InerButton>
+    </LoginForm>
+  );
 };
 export default LoginInput;
 
-
 const LoginForm = styled.div`
-    width: 70%;
-    left: 18%;
-    height: 220px;
-    position: relative;
-    margin-top: 50px;
+  width: 70%;
+  left: 18%;
+  height: 220px;
+  position: relative;
+  margin-top: 50px;
 
-  
-    & input {
-      width: 80%;
-      height: 45px;
-      border: none;
-      font-size: ${fontsize[0]};
-      position: absolute;
-      left: 25px;
-    
-      :-webkit-autofill {
-        -webkit-box-shadow: 0 0 0 1000px #ffff inset;
-      }
+  & input {
+    width: 80%;
+    height: 45px;
+    border: none;
+    font-size: ${fontsize[0]};
+    position: absolute;
+    left: 25px;
+
+    :-webkit-autofill {
+      -webkit-box-shadow: 0 0 0 1000px #ffff inset;
     }
+  }
 
   & input::placeholder {
     ${InnerPlaceHolder}
@@ -116,41 +113,37 @@ const LoginForm = styled.div`
   & input:focus {
     outline: none;
   }
-`
+`;
 const Inputbox = styled.div`
- width: 80%;
-    height: 50px;
-    margin-bottom: 30px;
-    border: none;
-    border-bottom: 4px solid #afafaf;
-    font-size: ${fontsize[0]};
-    display: flex;
-    position: relative;
-    left: 5%;
-    border-radius: 10px;
-    background-Color:white;
-`
-
+  width: 80%;
+  height: 50px;
+  margin-bottom: 30px;
+  border: none;
+  border-bottom: 4px solid #afafaf;
+  font-size: ${fontsize[0]};
+  display: flex;
+  position: relative;
+  left: 5%;
+  border-radius: 10px;
+  background-color: white;
+`;
 
 const InerButton = styled.button`
   width: 70%;
   height: 50px;
   border-radius: 15px;
-  background-color: #7C83FD;
+  background-color: #7c83fd;
   border: none;
   color: white;
-  font-family: "Gaegu", serif;
+  font-family: 'Gaegu', serif;
   position: relative;
-  bottom:5px;
+  bottom: 5px;
   left: 10%;
   text-align: center;
   font-size: ${fontsize[3]};
   cursor: pointer;
   margin: 0 auto;
-  :hover{
+  :hover {
     background-color: #5f67f8;
   }
-
-
-  
-`
+`;
